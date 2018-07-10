195586ea2942d17529b2701f1b91a9b5165192c5
Brandon Kamaka <Bkamaka@gmail.com>
2018-07-10 13:30:42 -0400

    Refactored to use an anonymous map.

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <sys/errno.h>
+#include <string.h>
 
 int main(){
 
@@ -10,12 +12,17 @@ int main(){
     //the parent should then print the message.
 
     const int SIZE = 4096;
-    const char* NAME = "sh_buffer";
-
-    int sh_fd;
     void* ptr;
-    pid_t pid_a;
 
+    ptr = mmap( NULL, SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0 );
+
+    if( ptr == MAP_FAILED){
+
+        printf( "Map Failed: %s", strerror(errno) );
+
+    }
+
+    pid_t pid_a;
     pid_a = fork();
 
     switch( pid_a ){
@@ -29,25 +36,42 @@ int main(){
         case 0:
 
             //child process
+
+            /*
+             * This is the book method to create a general shared map
+             * however, given that this is a child process, we can skip almost all of it
+             * in favor of using an anonymous map that is not file-backed
+
+            //Create the file descriptor
             sh_fd = shm_open( NAME, O_CREAT | O_RDWR, 0666 );
+
+            //size it
             ftruncate( sh_fd, SIZE );
-            ptr = mmap( NULL, SIZE, PROT_READ, MAP_SHARED, sh_fd, 0 );
+
+            //grab a pointer to it.
+            ptr = mmap( NULL, SIZE, PROT_WRITE, MAP_SHARED, sh_fd, 0 );
+            */
+
 
             //TODO: implement buffer pointer
-            sprintf( (char*) ptr, "%s", "test" );
+            sprintf( (char*) ptr, "%s", "I am awesome" );
             break;
 
         default:
 
-            //parent process
-            sh_fd = shm_open( NAME, O_CREAT | O_RDWR, 0666 );
-            ftruncate( sh_fd, SIZE );
-            ptr = mmap( NULL, SIZE, PROT_READ, MAP_SHARED, sh_fd, 0 );
-
             wait( &pid_a );
 
             //TODO: Change to a file write, later and implement a buffer counter
-            printf( "%s", (char*) ptr );
+            printf( "test: %s\n", (char*) ptr );
+
+            int rtn = munmap( ptr, SIZE);
+
+            if (0 != rtn){
+
+                printf( "Error unmapping memory: %s", strerror(errno) );
+
+            }
+
 
             break;
     }
